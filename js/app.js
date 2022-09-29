2ade55949feb6d78a9052de1a4d200ec6e82991d
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2022-09-29 23:43:40 +0600

    modal url added

--- js/app.js
+++ js/app.js
@@ -17,32 +17,48 @@ const displayNews = newses => {
     })
 }
 
-const newNewsLoad = () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+const newNewsLoad = (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayNewNewsLoad(data.data))
 }
 
 const displayNewNewsLoad = catagories => {
-    const newsContainer = document.getElementById('new-container');
-    newsContainer.innerHTML = '';
+    const displayNewsContainer = document.getElementById('new-container');
+    displayNewsContainer.innerHTML = '';
+    const countContainer = document.getElementById('detail-news');
+    countContainer.innerHTML = '';
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <h1>${catagories.length} items found for category Entertainment</h1>
+    `;
+    countContainer.appendChild(div);
     catagories.forEach(items => {
-        console.log(items)
+        // console.log(items)
         const detailDiv = document.createElement('div');
         detailDiv.classList.add('card', 'lg:card-side', 'bg-base-100', 'shadow-xl', 'mt-5')
         detailDiv.innerHTML = `
         <figure><img src="${items.thumbnail_url}" alt="Album"/></figure>
         <div class="card-body">
           <h2 class="card-title">${items.title}</h2>
-          <p>${items.details}</p>
+          <p>${items.details.slice(0, 400) + '...'}</p>
+          <p></p>
           <div class="card-actions justify-end">
-            <button class="btn btn-primary">Listen</button>
+            <button onclick="modalDetails('${items._id}')" class="btn btn-primary btn-sm">Details</button>
           </div>
         </div>
         `;
-        newsContainer.appendChild(detailDiv);
+        displayNewsContainer.appendChild(detailDiv);
     })
 }
+
+const modalDetails = (id) => {
+    // console.log(id)
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+    .then(res => res.json())
+    .then(data => console.log(data.data[0]))
+}
+
 
 newsLoad();
