ba76cc700166082d1b1f89faa1d9faa580b38337
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2022-09-29 20:27:48 +0600

    new-news added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const newsLoad = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
@@ -8,14 +7,23 @@ const newsLoad = () => {
 const displayNews = newses => {
     const newsContainer = document.getElementById('news-container');
     newses.forEach(news => {
-        console.log(news);
+        // console.log(news);
         const newsDiv = document.createElement('div');
         newsContainer.classList.add('flex', 'justify-around', 'mt-12')
         newsDiv.innerHTML = `
-           <button class="btn btn-sm">${news.category_name}</button>
+           <button onclick = "newNewsLoad(${news.category_id})" class="btn btn-sm">${news.category_name}</button>
         `;
         newsContainer.appendChild(newsDiv);
     })
 }
+
+const newNewsLoad = () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayNewNewsLoad(data.data))
+}
+
+
 
 newsLoad();
